259a07282f534f6e3cf80d63bf9add76bfe6f2f4
JakeSCahill <jake@redpanda.com>
2023-06-12 18:48:45 +0100

    Do not override latest console version

--- extensions/version-fetcher/set-latest-version.js
+++ extensions/version-fetcher/set-latest-version.js
@@ -11,38 +11,38 @@ module.exports.register = function ({ config }) {
   const logger = this.getLogger('set-latest-version-extension')
   if (!process.env.GITHUB_TOKEN) return
   this
-    .on('contentClassified', async ({ playbook, contentCatalog }) => {
-        const LatestRedpandaVersion = await GetLatestRedpandaVersion();
-        const LatestConsoleVersion = await GetLatestConsoleVersion();
-        if (!playbook.asciidoc) {
-          playbook.asciidoc = {};
-        }
+    .on('playbookBuilt', async ({ playbook }) => {
+      const LatestConsoleVersion = await GetLatestConsoleVersion();
+      if (!playbook.asciidoc) {
+        playbook.asciidoc = {};
+      }
 
-        if (!playbook.asciidoc.attributes) {
-          playbook.asciidoc.attributes = {};
-        }
+      if (!playbook.asciidoc.attributes) {
+        playbook.asciidoc.attributes = {};
+      }
+      playbook.asciidoc.attributes['latest-console-version'] = LatestConsoleVersion
+      console.log(`Set Redpanda Console version to ${LatestConsoleVersion}`);
+    })
+    .on('contentClassified', async ({ contentCatalog }) => {
+      const LatestRedpandaVersion = await GetLatestRedpandaVersion();
+      const components = await contentCatalog.getComponents();
+      for (let i = 0; i < components.length; i++) {
+        let component = components[i];
+        if (LatestRedpandaVersion.length !== 2) logger.warn('Could not find the latest Redpanda versions - using defaults');
+        if (component.name === 'ROOT') {
 
-        playbook.asciidoc.attributes['latest-console-version'] = LatestConsoleVersion;
-        console.log(`Set Redpanda Console version to ${LatestConsoleVersion}`);
-        const components = await contentCatalog.getComponents();
-        for (let i = 0; i < components.length; i++) {
-          let component = components[i];
-          if (LatestRedpandaVersion.length !== 2) logger.warn('Could not find the latest Redpanda versions - using defaults');
-          if (component.name === 'ROOT') {
-
-            if (!component.latest.asciidoc) {
-              component.latest.asciidoc = {};
-            }
-
-            if (!component.latest.asciidoc.attributes) {
-              component.latest.asciidoc.attributes = {};
-            }
-
-            component.latest.asciidoc.attributes['full-version'] = `${LatestRedpandaVersion[0]}`;
-            component.latest.asciidoc.attributes['latest-release-commit'] = `${LatestRedpandaVersion[1]}`;
-            console.log(`Set Redpanda version to ${LatestRedpandaVersion[0]} ${LatestRedpandaVersion[1]}`)
-            return components;
+          if (!component.latest.asciidoc) {
+            component.latest.asciidoc = {};
           }
+
+          if (!component.latest.asciidoc.attributes) {
+            component.latest.asciidoc.attributes = {};
+          }
+
+          component.latest.asciidoc.attributes['full-version'] = `${LatestRedpandaVersion[0]}`;
+          component.latest.asciidoc.attributes['latest-release-commit'] = `${LatestRedpandaVersion[1]}`;
+          console.log(`Set Redpanda version to ${LatestRedpandaVersion[0]} ${LatestRedpandaVersion[1]}`)
         }
-      })
+      }
+    })
 }
