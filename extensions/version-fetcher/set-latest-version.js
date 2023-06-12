7da68fa20e2730d0366386549a74e7781f5b0e00
JakeSCahill <jake@redpanda.com>
2023-06-12 17:42:01 +0100

    Handle edge cases in version fetcher

--- extensions/version-fetcher/set-latest-version.js
+++ extensions/version-fetcher/set-latest-version.js
@@ -14,13 +14,30 @@ module.exports.register = function ({ config }) {
     .on('contentClassified', async ({ playbook, contentCatalog }) => {
         const LatestRedpandaVersion = await GetLatestRedpandaVersion();
         const LatestConsoleVersion = await GetLatestConsoleVersion();
-        playbook.asciidoc.attributes['latest-console-version'] = LatestConsoleVersion
-        console.log(`Set Redpanda Console version to ${LatestConsoleVersion}`)
+        if (!playbook.asciidoc) {
+          playbook.asciidoc = {};
+        }
+
+        if (!playbook.asciidoc.attributes) {
+          playbook.asciidoc.attributes = {};
+        }
+
+        playbook.asciidoc.attributes['latest-console-version'] = LatestConsoleVersion;
+        console.log(`Set Redpanda Console version to ${LatestConsoleVersion}`);
         const components = await contentCatalog.getComponents();
         for (let i = 0; i < components.length; i++) {
           let component = components[i];
           if (LatestRedpandaVersion.length !== 2) logger.warn('Could not find the latest Redpanda versions - using defaults');
           if (component.name === 'ROOT') {
+
+            if (!component.latest.asciidoc) {
+              component.latest.asciidoc = {};
+            }
+
+            if (!component.latest.asciidoc.attributes) {
+              component.latest.asciidoc.attributes = {};
+            }
+
             component.latest.asciidoc.attributes['full-version'] = `${LatestRedpandaVersion[0]}`;
             component.latest.asciidoc.attributes['latest-release-commit'] = `${LatestRedpandaVersion[1]}`;
             console.log(`Set Redpanda version to ${LatestRedpandaVersion[0]} ${LatestRedpandaVersion[1]}`)
