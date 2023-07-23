663d7c1d4b8af9a26082d2042a399b2cccf82daf
JakeSCahill <jake@redpanda.com>
2023-07-23 19:47:12 +0100

    Apply console version to all component versions

--- extensions/version-fetcher/set-latest-version.js
+++ extensions/version-fetcher/set-latest-version.js
@@ -20,30 +20,34 @@ module.exports.register = function ({ config }) {
         const LatestRedpandaVersion = await GetLatestRedpandaVersion();
         const LatestConsoleVersion = await GetLatestConsoleVersion();
         if (LatestRedpandaVersion.length !== 2 || !LatestRedpandaVersion[0]) {
-          logger.warn('Failed to get the latest Redpanda versions - using defaults');
-          return
+          logger.warn('Failed to get the latest Redpanda version - using defaults');
         }
         if (!LatestConsoleVersion) {
           logger.warn(`Failed to get latest Console version from GitHub - using default`)
-          return
         }
         const components = await contentCatalog.getComponents();
         for (let i = 0; i < components.length; i++) {
           let component = components[i];
-          if (component.name === 'ROOT' || component.name === 'preview') {
+          if (component.name !== 'ROOT' || component.name !== 'preview') continue
 
-            if (!component.latest.asciidoc) {
-              component.latest.asciidoc = {};
+          if (!component.latest.asciidoc) {
+            component.latest.asciidoc = {};
+          }
+
+          if (!component.latest.asciidoc.attributes) {
+            component.latest.asciidoc.attributes = {};
+          }
+
+          if (LatestRedpandaVersion.length !== 2 || !LatestRedpandaVersion[0]) continue;
+
+          component.latest.asciidoc.attributes['full-version'] = `${LatestRedpandaVersion[0]}`;
+          component.latest.asciidoc.attributes['latest-release-commit'] = `${LatestRedpandaVersion[1]}`;
+          console.log(`${chalk.green('Set Redpanda version to')} ${chalk.bold(LatestRedpandaVersion[0])} ${chalk.bold(LatestRedpandaVersion[1])}`)
+
+          if (LatestConsoleVersion) {
+            for (const { asciidoc } of component.versions) {
+              asciidoc.attributes['latest-console-version'] = `${LatestConsoleVersion}`;
             }
-
-            if (!component.latest.asciidoc.attributes) {
-              component.latest.asciidoc.attributes = {};
-            }
-
-            component.latest.asciidoc.attributes['full-version'] = `${LatestRedpandaVersion[0]}`;
-            component.latest.asciidoc.attributes['latest-release-commit'] = `${LatestRedpandaVersion[1]}`;
-            console.log(`${chalk.green('Set Redpanda version to')} ${chalk.bold(LatestRedpandaVersion[0])} ${chalk.bold(LatestRedpandaVersion[1])}`)
-            component.latest.asciidoc.attributes['latest-console-version'] = `${LatestConsoleVersion}`;
             console.log(`${chalk.green('Set Redpanda Console version to')} ${chalk.bold(LatestConsoleVersion)}`);
           }
         }
