789068e319503b9f0e37018056fe04c30332642a
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2024-06-06 17:52:08 +0100

    Add the latest release tag for redpanda-data/connect to playbook (#49)
    * Fetch the latest release tag for redpanda-data/connect and update the playbook to get content from that tag
    
    * Update extensions/modify-connect-tag-playbook.js
    
    Co-authored-by: Paulo Borges <paulohtb@hotmail.com>
    
    * Only fetch the tag if the connect source is available
    
    ---------
    
    Co-authored-by: Paulo Borges <paulohtb@hotmail.com>

---
+++ extensions/modify-connect-tag-playbook.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const GetLatestConnectTag = require('./version-fetcher/get-latest-connect');
+
+module.exports.register = function ({ config }) {
+  const logger = this.getLogger('modify-connect-tag-playbook-extension');
+
+  this.on('contextStarted', async ({ playbook }) => {
+    try {
+      // Fetch the latest release tag for redpanda-data/connect
+      const sourceUrl = 'https://github.com/redpanda-data/connect';
+      if (playbook && playbook.content && playbook.content.sources) {
+        const source = playbook.content.sources.find(source => source.url === sourceUrl);
+        if (source) {
+          const latestTag = await GetLatestConnectTag();
+          if (latestTag) {
+            source.tags[0] = latestTag;
+            this.updateVariables({ playbook });
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Failed to update playbook with the latest Redpanda Connect tag:', error);
+    }
+  });
+}
---
+++ extensions/version-fetcher/get-latest-connect.js
@@ -0,0 +1,28 @@
+// Fetch the latest release version from GitHub
+const { Octokit } = require("@octokit/rest");
+const { retry } = require("@octokit/plugin-retry");
+const OctokitWithRetries = Octokit.plugin(retry);
+const owner = 'redpanda-data';
+const repo = 'connect';
+
+let githubOptions = {
+  userAgent: 'Redpanda Docs',
+  baseUrl: 'https://api.github.com',
+};
+
+if (process.env.REDPANDA_GITHUB_TOKEN) {
+  githubOptions.auth = process.env.REDPANDA_GITHUB_TOKEN;
+}
+
+const github = new OctokitWithRetries(githubOptions);
+
+module.exports = async () => {
+  try {
+    const release = await github.rest.repos.getLatestRelease({ owner, repo });
+    tag = release.data.tag_name;
+    return tag;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
