a373f0aa71408806d8d7ca7d79162820e59c2692
JakeSCahill <jake@redpanda.com>
2024-06-10 16:26:12 +0100

    Unpublish beta pages when beta docs go GA

--- extensions/unpublish-pages.js
+++ extensions/unpublish-pages.js
@@ -1,9 +1,32 @@
+'use strict';
+
 module.exports.register = function () {
   this.on('documentsConverted', ({ contentCatalog }) => {
-    contentCatalog.getPages((page) => page.out).forEach((page) => {
-      if (page.asciidoc?.attributes['page-unpublish'] != null || page.asciidoc?.attributes['page-layout'] === 'api-partial') {
-        delete page.out
+    // Get all pages that have an `out` property
+    const pages = contentCatalog.getPages((page) => page.out);
+    let componentName;
+
+    // Iterate over each page
+    pages.forEach((page) => {
+      // Retrieve the component associated with the page
+      componentName = page.asciidoc?.attributes['page-component-name'];
+      const component = contentCatalog.getComponent(componentName);
+
+      // Check if the component has a latest prerelease
+      const prerelease = component ? component.latestPrerelease : null;
+
+      // Check the conditions for unpublishing the page
+      const shouldUnpublish = (
+        page.asciidoc?.attributes['page-unpublish'] ||
+        (page.asciidoc?.attributes['publish-only-during-beta']
+        && !prerelease // No beta version available, meaning the beta period has ended
+      )
+      );
+
+      // If any condition is met, unpublish the page
+      if (shouldUnpublish) {
+        delete page.out;
       }
-    })
-  })
-}
+    });
+  });
+};
