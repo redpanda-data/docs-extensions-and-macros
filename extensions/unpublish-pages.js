42e612537ffbd45aa5ffc1e9958453147fe5d9c2
JakeSCahill <jake@redpanda.com>
2024-06-26 15:55:49 +0100

    Unpublish beta pages that aren't in a beta version

--- extensions/unpublish-pages.js
+++ extensions/unpublish-pages.js
@@ -5,26 +5,27 @@ module.exports.register = function () {
     // Get all pages that have an `out` property
     const pages = contentCatalog.getPages((page) => page.out);
     let componentName;
+    let pageVersion;
     siteCatalog.unpublishedPages = []
 
-    // Iterate over each page
     pages.forEach((page) => {
-      // Retrieve the component associated with the page
+      // Retrieve the component metadata associated with the page
       componentName = page.asciidoc?.attributes['page-component-name'];
+      pageVersion = page.asciidoc?.attributes['page-component-version'];
       const component = contentCatalog.getComponent(componentName);
 
-      // Check if the component has a latest prerelease
-      const prerelease = component && component.latestPrerelease ? component.latestPrerelease : false;
+      // Check if the page is part of the beta version
+      const isPrerelease = component && pageVersion && component.latestPrerelease && component.latestPrerelease.version === pageVersion;
 
       // Check the conditions for unpublishing the page
       const shouldUnpublish = (
         page.asciidoc?.attributes['page-unpublish'] ||
         (page.asciidoc?.attributes['publish-only-during-beta']
-        && !prerelease // No beta version available, meaning the beta period has ended
+        && !isPrerelease // No beta version available, meaning the beta period has ended
       )
       );
 
-      // If any condition is met, unpublish the page
+      // Unpublish the shouldUnpublish pages
       if (shouldUnpublish) {
         siteCatalog.unpublishedPages.push(page.pub.url)
         delete page.out;
