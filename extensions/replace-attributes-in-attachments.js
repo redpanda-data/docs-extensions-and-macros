6797b97420e9deff708a5d76c042dd0d6069e35d
JakeSCahill <jake@redpanda.com>
2024-06-27 10:52:55 +0100

    Ensure that val is a string

--- extensions/replace-attributes-in-attachments.js
+++ extensions/replace-attributes-in-attachments.js
@@ -2,7 +2,6 @@
 
 module.exports.register = function () {
   this.on('contentClassified', ({ contentCatalog }) => {
-    // Create a lookup table for component versions
     const componentVersionTable = contentCatalog.getComponents().reduce((componentMap, component) => {
       componentMap[component.name] = component.versions.reduce((versionMap, componentVersion) => {
         versionMap[componentVersion.version] = componentVersion;
@@ -11,21 +10,15 @@ module.exports.register = function () {
       return componentMap;
     }, {});
 
-    // Process each attachment
     contentCatalog.findBy({ family: 'attachment' }).forEach((attachment) => {
-      // Get the corresponding component version
-      const componentVersion = componentVersionTable[attachment.src.component]?.[attachment.src.version];
-      if (!componentVersion) return;
-
+      const componentVersion = componentVersionTable[attachment.src.component][attachment.src.version];
       let attributes = componentVersion.asciidoc?.attributes;
       if (!attributes) return;
-
-      // Remove trailing '@' from attribute values
       attributes = Object.entries(attributes).reduce((accum, [name, val]) => {
-        accum[name] = val && val.endsWith('@') ? val.slice(0, val.length - 1) : val;
+        const stringValue = String(val); // Ensure val is a string
+        accum[name] = stringValue.endsWith('@') ? stringValue.slice(0, stringValue.length - 1) : stringValue;
         return accum;
       }, {});
-
       let modified;
       const result = attachment.contents.toString().replace(/\{([\p{Alpha}\d_][\p{Alpha}\d_-]*)\}/gu, (match, name) => {
         if (!(name in attributes)) return match;
@@ -34,8 +27,6 @@ module.exports.register = function () {
         if (value.endsWith('@')) value = value.slice(0, value.length - 1);
         return value;
       });
-
-      // Update the attachment contents if modified
       if (modified) attachment.contents = Buffer.from(result);
     });
   });
