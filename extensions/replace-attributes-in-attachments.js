cbd4c93213ad5d7bce100db50a60a9e0d05e4beb
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2024-07-12 16:05:42 +0100

    Fix replacer extension to replace env vars with versions (#61)
    * Fix replacer extension to replace env vars with versions
    
    * Handle beta versions

--- extensions/replace-attributes-in-attachments.js
+++ extensions/replace-attributes-in-attachments.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports.register = function () {
+  const sanitizeAttributeValue = (value) => String(value).replace("@", "");
   this.on('contentClassified', ({ contentCatalog }) => {
     const componentVersionTable = contentCatalog.getComponents().reduce((componentMap, component) => {
       componentMap[component.name] = component.versions.reduce((versionMap, componentVersion) => {
@@ -20,13 +21,30 @@ module.exports.register = function () {
         return accum;
       }, {});
       let modified;
-      const result = attachment.contents.toString().replace(/\{([\p{Alpha}\d_][\p{Alpha}\d_-]*)\}/gu, (match, name) => {
+      let contentString = attachment.contents.toString();
+      // Specific replacements for YAML files
+      if (attachment.out.path.endsWith('.yaml') || attachment.out.path.endsWith('.yml')) {
+        const redpandaVersionRegex = /(\$\{REDPANDA_VERSION[^\}]*\})/g;
+        const redpandaConsoleVersionRegex = /(\$\{REDPANDA_CONSOLE_VERSION[^\}]*\})/g;
+        let fullVersion = attributes['full-version'] ? sanitizeAttributeValue(attributes['full-version']) : '';
+        const latestConsoleVersion = attributes['latest-console-version'] ? sanitizeAttributeValue(attributes['latest-console-version']) : '';
+
+        if (attributes['page-component-version-is-prerelease'] === 'true') {
+          fullVersion = attributes['redpanda-beta-version'] ? sanitizeAttributeValue(attributes['redpanda-beta-version']) : fullVersion;
+        }
+
+        contentString = contentString.replace(redpandaVersionRegex, fullVersion);
+        contentString = contentString.replace(redpandaConsoleVersionRegex, latestConsoleVersion);
+      }
+
+      const result = contentString.replace(/\{([\p{Alpha}\d_][\p{Alpha}\d_-]*)\}/gu, (match, name) => {
         if (!(name in attributes)) return match;
         modified = true;
         let value = attributes[name];
         if (value.endsWith('@')) value = value.slice(0, value.length - 1);
         return value;
       });
+
       if (modified) attachment.contents = Buffer.from(result);
     });
   });
