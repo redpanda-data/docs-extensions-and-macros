eda04cf54a4ea1c401a6140a206e3c19426493ff
Paulo Borges <paulohtb@hotmail.com>
2024-09-09 14:51:38 -0300

    Improvements to csv (#72)
    * if type is two words, replace the _ with space
    
    * sort support level, and only show the different names if there's multiple
    
    * modify data structure
    
    * add utilities folder
    
    * adjust to new data structure

---
+++ extensions/util/customStringify.js
@@ -0,0 +1,19 @@
+function customStringify(obj) {
+    return JSON.stringify(obj, (key, value) => {
+        if (value instanceof Map) {
+            return {
+                type: 'Map',
+                value: Array.from(value.entries())
+            };
+        } else if (value instanceof Set) {
+            return {
+                type: 'Set',
+                value: Array.from(value)
+            };
+        } else if (typeof value === 'function') {
+            return value.toString();
+        } else {
+            return value;
+        }
+    }, 2);
+}
