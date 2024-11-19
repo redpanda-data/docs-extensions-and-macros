61dcf4bfa1d6f623c93b67cfc58e7a7460223e11
JakeSCahill <jake@redpanda.com>
2024-11-19 11:40:22 +0000

    Add support for hosting files at the site root

---
+++ extensions/add-pages-to-root.js
@@ -0,0 +1,25 @@
+module.exports.register = function ({ config }) {
+  this.on('beforePublish', ({ siteCatalog, contentCatalog }) => {
+    const logger = this.getLogger('add-pages-to-site-root');
+
+    logger.debug('Files to process:', config.files);
+
+    config.files.forEach(filePath => {
+      const resource = contentCatalog.resolveResource(filePath);
+      if (resource) {
+        const basename = resource.src.basename; // Get the file's basename
+        const contentsBuffer = resource.contents; // Access the file's contents as a Buffer
+        logger.debug(`Processing file: ${basename}`);
+        // Add the file to the root directory in the site catalog
+        siteCatalog.addFile({
+          contents: contentsBuffer,
+          out: {
+            path: basename, // Add the file to the root directory
+          },
+        });
+      } else {
+        logger.warn(`File not resolved: ${filePath}`);
+      }
+    });
+  });
+};
